6c29c9666eeb1f43080213f517ae9e4848ec8059
Diego Guedes <digm10@hotmail.com>
2022-08-25 20:39:01 -0300

    commit inicial, desafio recursivo

---
+++ 25082022paa2/desafioRecursivo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void geraSequencia(int a[], int n, int s[], int i);
+
+int main(){
+	geraSequencia();
+}
+
+void geraSequencia(int a[], int n, int s[], int i){
+	if(i > n){
+		printf(s,n);
+		return;
+	}else{
+}
