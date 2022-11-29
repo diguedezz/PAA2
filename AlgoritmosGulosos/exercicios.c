4e87fa54af69c43a6f54a1ad0f68ee3d188a8132
unknown <digm10@hotmail.com>
2022-11-29 01:33:10 -0300

    Create exercicios.c

---
+++ AlgoritmosGulosos/exercicios.c
@@ -0,0 +1,52 @@
+/*
+Universidade Presbiteriana Mackenzie
+Faculdade de Computação e Informática
+Ciência da Computação
+Projeto e Análise de Algoritmos II
+
+São Paulo, 29 de novembro de 2022.
+Diego Guedes de Moraes.
+
+
+Exercícios propostos sobre Algoritmos Gulosos
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Algoritmo Seleção de Atividades
+Complexidade: O(n) + O(n log n)(ordenação)
+
+Exercícios:
+*/
+
+//3) Execute o algoritmo SelecaoAtividades(A) o conjunto de atividades especificadas pelos pares (si,fi) descritos abaixo, onde si é o tempo de início da atividade e fi o tempo de término da atividade :
+//A={ (7,9), (5.6), (4,9), (1,2), (3,7), (6,8), (2,5), (1,3), (1,4)}
+
+//4) Considere a estratégia a seguir para o problema de Seleção de Atividade: A cada iteração escolher uma atividade compatível que comece primeiro. Essa estratégia produz uma solução ótima?
+
+//5) Implemente versão recursiva do o algoritmo SelecionaAtividades(A) na linguagem C.
+
+
+
+/*
+Código de Huffman
+Complexidade: O(n)(inserção) 
+Total: O(n²), caso em heap: O(logn) e O(nlogn)
+
+Exercícios:
+*/
+//6) Determine os códigos e as árvores de Huffman para um texto com os seguintes caracteres e frequências:
+//a) a = 7, b = 5, c = 10, d = 21, e = 90, f = 11, g = 7 e h = 2;
+//b) a = 1, b = 1, c = 2, d = 3, e = 5, f = 8, g = 13 e h = 21;
+//c) a = 45, b = 13, c = 12, d = 16, e = 9, f = 5.
+
+//7) Descreva a árvore de Huffman quando as frequências são os primeiros n números de Fibonacci. Pra começar teste para n = 10.
+
+//8) Escreva uma função que tendo como entrada uma sequência de caracteres, o seu algoritmo gera uma tabela de frequência de caracteres gastando tempo O(n), onde n é o tamanho da sequência de caracteres.
+
+//9) Implemente na linguagem C o algoritmo de Huffman apresentando, considere que o algoritmo tem como entrada uma lista de caracteres e com suas frequências e como saída a árvore binária de Huffman.
+
+
+
